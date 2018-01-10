657458918eb6dd56c596f333a122bde429d5f10a
Hanifa Fissalma <halimiliul@gmail.com>
2018-01-10 09:38:07 +0700

    update

--- src/TambahOrganisasi.js
+++ src/TambahOrganisasi.js
@@ -2,15 +2,22 @@ import React, {Component} from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 class TambahOrganisasi extends Component{
   constructor(){
     super();
     this.state={
-      open:false
+      open:false,
+      jenis_organisasi:1,
+      jenjang_organisasi:1
     }
     this.handleOpen=this.handleOpen.bind(this);
     this.handleClose=this.handleClose.bind(this);
     this.handleSubmit=this.handleSubmit.bind(this);
+    this.handleJenisOrganisasiChange=this.handleJenisOrganisasiChange.bind(this);
+    this.handleJenjangOrganisasiChange=this.handleJenjangOrganisasiChange.bind(this);
   }
 
   handleOpen(){
@@ -25,6 +32,12 @@ class TambahOrganisasi extends Component{
 
   }
 
+  handleJenisOrganisasiChange(event,index,value){
+    this.setState({jenis_organisasi:value});
+  }
+  handleJenjangOrganisasiChange(event,index,value){
+    this.setState({jenjang_organisasi:value});
+  }
   render(){
     const actions = [
       <FlatButton
@@ -48,7 +61,64 @@ class TambahOrganisasi extends Component{
          open={this.state.open}
          autoScrollBodyContent={true}
        >
-        bla bla bla
+        <form id="form_ubah_biodata">
+          <TextField
+            style={{width:'100%'}}
+            name="nama_organisasi"
+            type="text"
+            required="required"
+            floatingLabelText="Nama Organisasi"
+          />
+          <SelectField
+              style={{width:'100%'}}
+              value={this.state.jenis_organisasi}
+              onChange={this.handleJenisOrganisasiChange}
+              floatingLabelText="Jenis Organisasi"
+              floatingLabelFixed={true}
+              hintText="Pilih Jenis Organisasi"
+              name="jenis_organisasi"
+              required="required"
+            >
+              <MenuItem value={1} primaryText="--Pilih Jenis Organisasi--" />
+              <MenuItem value={2} primaryText="ORMAWA" />
+              <MenuItem value={3} primaryText="OPMAWA" />
+          </SelectField>
+          <SelectField
+              style={{width:'100%'}}
+              value={this.state.jenjang_organisasi}
+              onChange={this.handleJenjangOrganisasiChange}
+              floatingLabelText="Jenjang Organisasi"
+              floatingLabelFixed={true}
+              hintText="Pilih Jenjang Organisasi"
+              name="jenjang_organisasi"
+              required="required"
+            >
+              <MenuItem value={1} primaryText="--Pilih Jenjang Organisasi--" />
+              <MenuItem value={2} primaryText="Program Studi" />
+              <MenuItem value={3} primaryText="Fakultas" />
+              <MenuItem value={4} primaryText="Universitas" />
+          </SelectField>
+          <TextField
+            style={{width:'100%'}}
+            name="jabatan"
+            type="text"
+            required="required"
+            floatingLabelText="Posisi / Jabatan"/>
+          <TextField
+            style={{width:'100%'}}
+            name="deskripsi"
+            type="text"
+            required="required"
+            floatingLabelText="Deskripsi"
+          />
+          <RaisedButton
+            style={{width:'35%'}}
+            containerElement='label'
+          >
+            <input type="file" />
+          </RaisedButton>
+            
+        </form>
        </Dialog>
      </div>
     )
